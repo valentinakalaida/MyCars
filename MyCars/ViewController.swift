c4504e7e97d438a7bc59073e363ab33b9aeede99
Valentina Kalaida <valen2302@ukr.net>
2020-02-10 09:05:15 +0200

    Initial Commit

---
+++ MyCars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Валентина Калайда on 10.02.2020.
+//  Copyright © 2020 Валентина Калайда. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
